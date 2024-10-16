b5b78f653cc669721ff334bc12da3d10db46f967
Awni-android <awni.hub99@gmail.com>
2024-10-17 02:26:35 +0300

    first server

---
+++ index.js
@@ -0,0 +1,17 @@
+import http from "http";
+import { waitForDebugger } from "inspector";
+
+const host = "localhost";
+const port = 8000;
+
+const requestListener = function (req, res) {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello World!");
+};
+
+const server = http.createServer(requestListener);
+
+server.listen(port, host, () => {
+  console.log(`Server is running on http://${host}:${port}`);
+});
